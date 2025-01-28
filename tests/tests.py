5d61ca8a4f6246b4ed061f00313f93d2c0de7c8b
AlexanderBeli <alexandrbelitskiy@gmail.com>
2025-01-28 22:39:47 +0100

    tests added to tests.py

--- tests/tests.py
+++ tests/tests.py
@@ -1,17 +1,15 @@
 import unittest
-from unittest.mock import Mock, patch, MagicMock, mock_open
 from dataclasses import asdict
+from unittest import mock
 
-from src.application.use_case.create_book_use_case import CreateBookUseCase
 from src.application.ports.information_input_port import InformationInputPort
+from src.application.use_case.create_book_use_case import CreateBookUseCase
 from src.domain.entity.book import Book
 from src.infrastructure.repository.json_data_file_operations import JsonDataFile
 from src.infrastructure.repository.library_extra_operations import Library
 
 
 class TestCreateBookUseCase(unittest.TestCase):
-
-
 
     def setUp(self):
         # self.mock_storage = MagicMock(spec=JsonDataFile)
@@ -32,18 +30,44 @@ class TestCreateBookUseCase(unittest.TestCase):
         book_dict = asdict(book)
         self.assertEqual(book_dict["status"], "в наличии")
 
+    @mock.patch("os.path.exists", return_value=False)
+    @mock.patch("builtins.open", new_callable=mock.mock_open)
+    def test_add_to_dict_no_file(self, mock_file, mock_file_exists):
+        JsonDataFile().add_book_controller(book_id="1234", data=self.book_data)
+        mock_file_exists.assert_called_once_with(".data/biblioteque.json")
+        mock_file.assert_called_once_with(".data/biblioteque.json", "w")
+
+    @mock.patch("json.dump")
+    @mock.patch("os.path.exists", return_value=True)
+    @mock.patch("builtins.open", new_callable=mock.mock_open, read_data="{}")
+    def test_add_to_dict_has_file(
+        self,
+        mock_file_open: mock.MagicMock,
+        mock_file_exists: mock.MagicMock,
+        mock_jsondump: mock.MagicMock,
+    ):
+        JsonDataFile().add_book_controller(book_id="1234", data=self.book_data)
+        mock_file_exists.assert_called_once_with(".data/biblioteque.json")
+        self.assertEqual(
+            [
+                mock.call(".data/biblioteque.json", "r"),
+                mock.call(".data/biblioteque.json", "w"),
+            ],
+            mock_file_open.call_args_list,
+        )
+        expected_data = {"1234": self.book_data}
+        mock_jsondump.assert_called_once_with(expected_data, mock.ANY)
+
     # @patch("builtins.open", new_callable=mock_open)
     # def test_add_to_dict(self, mock_file):
     #     JsonDataFile().add_book_controller(book_id="1234", data=self.book_data)
 
-
-
     # def test_add_book(self):
-        # self.mock_storage.open_biblioteque.return_value = []
-        # book = Book("Book 1", "Author 1", "2000")
-        # book_dict = asdict(book)
-        # self.mock_storage.add_to_biblioteque({book_dict}).assert_called_once()
-        # self.assertEqual(self.mock_storage.add_to_biblioteque.call_args[0][0], [book_dict.__dict__])
+    # self.mock_storage.open_biblioteque.return_value = []
+    # book = Book("Book 1", "Author 1", "2000")
+    # book_dict = asdict(book)
+    # self.mock_storage.add_to_biblioteque({book_dict}).assert_called_once()
+    # self.assertEqual(self.mock_storage.add_to_biblioteque.call_args[0][0], [book_dict.__dict__])
 
     # @patch("builtins.input")
     # def test_add_the_book_information(self, mocked_input):
